a11bd90b65046cebec657f2601d9eeb0feffdab0
Валерия <vadrozdovskaja@mail.ru>
2023-07-07 23:40:40 +0300

    Добавлена задача 18

---
+++ Task018/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая по 
+// заданному номеру четверти, показывает диапозон 
+// возможных координат точек в этой четверти (x и y)
+
+Console.WriteLine("Введите номер четверти");
+int number = Convert.ToInt32(Console.ReadLine());
+
+string range = Range(number);
+string result = range == null
+                ? "Указана неверная четверть"
+                : $"Диапозон возможных координат точек в этой четверти -> {range}";
+
+
+Console.WriteLine (result);
+
+string Range(int num)
+{
+    if (num == 1) return "x > 0, y > 0";
+    if (num == 2) return "x < 0, y > 0";
+    if (num == 3) return "x < 0, y < 0";
+    if (num == 4) return "x > 0, y < 0";
+    return null;
+}
