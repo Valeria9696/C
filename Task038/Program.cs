8ace1a50befc06cb8455dc63e480ad03f6b4049e
Валерия <vadrozdovskaja@mail.ru>
2023-07-18 23:16:19 +0300

    добавлены комментарии к задаче 38

---
+++ Task038/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
+
+
+double[] CreateArrayRndInt(int size,int min, int max)
+{
+    double[] arr = new double [size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(double[] arr, string sep = ", ")
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}{sep}");
+        else Console.Write($"{arr[i]}");
+    }
+}
+
+double[] array = CreateArrayRndInt(5,-1, 99);
+Console.Write("[");
+PrintArray(array);
+Console.Write("]");
+Console.WriteLine();
